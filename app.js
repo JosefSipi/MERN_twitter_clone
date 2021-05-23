f3c5627cd3748f3ebe0c9d1317a09f8740ec87a9
Joseph <Josephsipiorski@gmail.com>
2021-05-23 16:10:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get("/", (req, res) => res.send("app.js"));
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
